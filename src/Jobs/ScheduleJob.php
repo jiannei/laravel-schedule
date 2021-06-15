e38debcde8d7fece30ae8954d05f57856469f7e7
jiannei <longjian.huang@aliyun.com>
2021-06-15 15:00:51 +0800

    fix: schedule job

--- src/Jobs/ScheduleJob.php
+++ src/Jobs/ScheduleJob.php
@@ -11,9 +11,25 @@
 
 namespace Jiannei\Schedule\Laravel\Jobs;
 
-use Illuminate\Queue\Jobs\Job;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
 use Jiannei\Schedule\Laravel\Contracts\ScheduleContract;
 
-abstract class ScheduleJob extends Job implements ScheduleContract
+class ScheduleJob implements ScheduleContract, ShouldQueue
 {
+    /*
+       |--------------------------------------------------------------------------
+       | Queueable Jobs
+       |--------------------------------------------------------------------------
+       |
+       | This job base class provides a central location to place any logic that
+       | is shared across all of your jobs. The trait included with the class
+       | provides access to the "queueOn" and "delay" queue helper methods.
+       |
+       */
+
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 }
