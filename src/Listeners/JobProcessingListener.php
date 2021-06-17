e5f5a29c4576244accd6afc2edfcb2908e8e7111
jiannei <Jiannei@users.noreply.github.com>
2021-06-17 02:32:11 +0000

    Apply fixes from StyleCI

--- src/Listeners/JobProcessingListener.php
+++ src/Listeners/JobProcessingListener.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of the jiannei/laravel-schedule.
+ *
+ * (c) jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jiannei\Schedule\Laravel\Listeners;
-
 
 use Illuminate\Queue\Events\JobProcessing;
 use Illuminate\Support\Carbon;
@@ -14,7 +21,7 @@ class JobProcessingListener
 
     public function handle(JobProcessing $event)
     {
-        if (!$this->schedulable($event->job)) {
+        if (! $this->schedulable($event->job)) {
             return;
         }
 
