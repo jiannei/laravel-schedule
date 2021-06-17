8207486ddbf411273b33a67703216d2fec363736
jiannei <Jiannei@users.noreply.github.com>
2021-06-17 02:32:11 +0000

    Apply fixes from StyleCI

--- src/Listeners/JobProcessedListener.php
+++ src/Listeners/JobProcessedListener.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of the jiannei/laravel-schedule.
+ *
+ * (c) jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jiannei\Schedule\Laravel\Listeners;
-
 
 use Illuminate\Queue\Events\JobProcessed;
 use Illuminate\Support\Carbon;
@@ -14,12 +21,12 @@ class JobProcessedListener
 
     public function handle(JobProcessed $event)
     {
-        if (!$this->schedulable($event->job)) {
+        if (! $this->schedulable($event->job)) {
             return;
         }
 
         $result = $this->resultModel()::query()->where('id', $event->job->getJobId())->first();
-        if (!$result) {
+        if (! $result) {
             return;
         }
 
--- src/Support/Helpers.php
+++ src/Support/Helpers.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of the jiannei/laravel-schedule.
+ *
+ * (c) jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jiannei\Schedule\Laravel\Support;
-
 
 use Illuminate\Contracts\Queue\Job;
 use Illuminate\Database\Eloquent\Model;
