2c7b2545ecd15aa9f368ca89591c2170519c164e
jiannei <Jiannei@users.noreply.github.com>
2021-06-19 00:17:10 +0000

    Apply fixes from StyleCI

--- src/Listeners/ScheduledTaskStartingListener.php
+++ src/Listeners/ScheduledTaskStartingListener.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of the jiannei/laravel-schedule.
+ *
+ * (c) jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jiannei\Schedule\Laravel\Listeners;
-
 
 use Illuminate\Console\Events\ScheduledTaskStarting;
 
@@ -10,6 +17,5 @@ class ScheduledTaskStartingListener
 {
     public function handle(ScheduledTaskStarting $event)
     {
-
     }
 }
