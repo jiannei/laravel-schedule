98cf97f1bbf90b5a571c5d48f20e5d1eb626f3cc
jiannei <Jiannei@users.noreply.github.com>
2021-06-19 00:17:10 +0000

    Apply fixes from StyleCI

--- src/Listeners/ScheduledTaskFinishedListener.php
+++ src/Listeners/ScheduledTaskFinishedListener.php
@@ -1,8 +1,15 @@
 <?php
 
+/*
+ * This file is part of the jiannei/laravel-schedule.
+ *
+ * (c) jiannei <longjian.huang@foxmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Jiannei\Schedule\Laravel\Listeners;
-
 
 use Illuminate\Console\Events\ScheduledTaskFinished;
 
@@ -10,6 +17,5 @@ class ScheduledTaskFinishedListener
 {
     public function handle(ScheduledTaskFinished $event)
     {
-
     }
 }
